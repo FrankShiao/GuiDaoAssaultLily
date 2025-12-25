5e9131667bf675d3b09a2709527ed6650d7bb0f5
紺野鈴涼 <afrank4299@gmail.com>
2025-12-25 11:18:08 +0800

    20251225 補上packages檔

--- Assets/Fungus/Scripts/Editor/ViewEditor.cs
+++ Assets/Fungus/Scripts/Editor/ViewEditor.cs
@@ -137,7 +137,7 @@ namespace Fungus.EditorUtils
 
             for (int i = 0; i < 2; ++i)
             {
-                var fmh_141_57_638758493023293330 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(handles[i],
+                var fmh_141_57_639022571580229887 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(handles[i],
                                                         HandleUtility.GetHandleSize(pos) * 0.1f,
                                                         Vector3.zero,
 #if UNITY_5_6_OR_NEWER
